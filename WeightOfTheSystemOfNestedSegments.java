766ab3c76643abce375c2d8b4da2ca7bf32282e9
rmakash999 <mrkbwithcode999@gmail.com>
2023-05-01 08:10:26 +0600

    -A

--- WeightOfTheSystemOfNestedSegments.java
+++ WeightOfTheSystemOfNestedSegments.java
@@ -1,27 +1,51 @@
 import java.util.*;
 
 public class WeightOfTheSystemOfNestedSegments {
-    static class Point{
+    
+
+    static class Point {
         int weight, position, id;
     }
-    public static void main(String[] args) {
+
+    static void solve() {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-->0){
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            Point[] points = new Point[m];
-            for (int i = 0; i< m; i++){
-                points[i] = new Point();
-                points[i].position = sc.nextInt();
-                points[i].weight = sc.nextInt();
-                points[i].id = i+1;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        Point[] points = new Point[m];
+
+        for (int i = 0; i < m; i++) {
+            points[i] = new Point();
+            points[i].position = sc.nextInt();
+            points[i].weight = sc.nextInt();
+            points[i].id = i + 1;
+        }
+
+        Arrays.sort(points, Comparator.comparingInt((Point a) -> a.weight));
+
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            if (i < 2 * n) {
+                sum += points[i].weight;
+            } else {
+                points = Arrays.copyOfRange(points, 0, i);
+                break;
             }
-            for (Point p :points){
-                System.out.println("Weight: " + p.weight + " Position: " + p.position + " Index: " + p.id );
-                
-            }
+        }
+
+        Arrays.sort(points, Comparator.comparingInt((Point a) -> a.position));
+
+        System.out.println(sum);
+        for (int i = 0; i < n; i++) {
+            System.out.println(points[i].id + " " + points[2 * n - i - 1].id);
         }
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        sc.close();
+    }
 }
